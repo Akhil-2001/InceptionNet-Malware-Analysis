58c0868662b4516df1614c5dd16458640dbab04b
akhil-2001 <akhil.mr2001@gmail.com>
2023-07-29 13:01:09 +0530

    removing unused imports

---
+++ test.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+device_lib.list_local_devices()
